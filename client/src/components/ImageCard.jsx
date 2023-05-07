432644215239ee5403ea4b323edd7052f9930b23
jitendra <jitendra.s1487@gmail.com>
2023-05-07 16:59:50 +0530

    disabling card selection

--- client/src/components/ImageCard.jsx
+++ client/src/components/ImageCard.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function ImageCard({ title, ImgData, description }) {
   return (
     <div className="bg-white h-full rounded-lg shadow-lg overflow-hidden flex">
-      <img src={`data:image/png;base64,${ImgData}`} alt={title} className="w-[100px] h-auto object-cover" />
+      <img src={`data:image/png;base64,${ImgData}`} alt={title} className="w-[100px] select-none h-auto object-cover" />
       <div className="p-6">
-      <h2 className="text-xl font-bold capitalize mb-2">{title}</h2>
-        <h2 className="text-sm font-medium capitalize mb-2">{description}</h2>
+      <h2 className="text-xl font-bold capitalize select-none mb-2">{title}</h2>
+        <h2 className="text-sm font-medium capitalize select-none mb-2">{description}</h2>
       </div>
     </div>
   );
